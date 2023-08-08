6e1232fd8ae27b4f1816e17f750dfb8363116fbe
mengleiz <menglei.zhang@intel.com>
2023-08-08 15:47:41 -0700

    update the start_ns3_env.py file

--- network_gym_env/start_ns3_env.py
+++ network_gym_env/start_ns3_env.py
@@ -8,12 +8,13 @@ from NetworkGymSim.network_gym_sim import build_ns3
 from NetworkGymSim.network_gym_sim import NetworkGymSim
 def main():
     """main function"""
-    build_ns3(config=False, build=True)
-    customEnv1 = CustomEnv('env1', NetworkGymSim, ['nqos_split', 'qos_steer', 'network_slicing'])
-    customEnv1.start()
-    #time.sleep(1)
-    #customEnv2 = CustomEnv('customEnv2', NetworkGymSim,  ['common', 'nqos_split', 'qos_steer', 'network_slicing'])
-    #customEnv2.start()
+    #build_ns3(config=False, build=False)
+
+    num_workers= 20
+    for worker in range(num_workers):
+        customEnv = CustomEnv('env'+str(worker), NetworkGymSim, ['nqos_split', 'qos_steer', 'network_slicing'])
+        customEnv.start()
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
